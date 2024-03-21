7e07c544720b1161ef93dcf5af5202f898b3460a
ZuhayrHalday <halday.zuhayr@gmail.com>
2024-03-22 01:52:08 +0200

    Added AVL Node Structure

--- src/GenericsKbAVLApp.java
+++ src/GenericsKbAVLApp.java
@@ -1,3 +1,21 @@
+class AVLNode {
+    String data;
+    AVLNode left;
+    AVLNode right;
+    int height;
+
+    /**
+     * Constructs an AVLNode with the given data.
+     *
+     * @param data The data to be stored in the node.
+     */
+    public AVLNode(String data) {
+        this.data = data;
+        left = right = null;
+        height = 1;
+    }
+}
+
 public class GenericsKbAVLApp {
     public static void main(String[] args) throws Exception {
         
