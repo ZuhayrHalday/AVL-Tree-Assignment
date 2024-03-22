fe2ffa4b2f372650a67ee88aeac95e5cdcc7f8bf
ZuhayrHalday <halday.zuhayr@gmail.com>
2024-03-22 02:06:53 +0200

    Added rotation functions for the AVL tree structure from lecture notes.

--- src/GenericsKbAVLApp.java
+++ src/GenericsKbAVLApp.java
@@ -27,6 +27,76 @@ class AVLTree {
     public AVLTree() {
         root = null;
     }
+
+    /**
+     * Calculates the height of a node.
+     *
+     * @param node The node to calculate the height for.
+     * @return The height of the node.
+     */
+    private int getHeight(AVLNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    /**
+     * Calculates the balance factor of a node.
+     *
+     * @param node The node to calculate the balance factor for.
+     * @return The balance factor of the node.
+     */
+    private int getBalanceFactor(AVLNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    /**
+     * Rotates a node on the AVL tree left
+     *
+     * @param y The node to perform the rotation on.
+     * @return The new root node after rotation.
+     */
+    private AVLNode rightRotate(AVLNode y) {
+        AVLNode x = y.left;
+        AVLNode swapVariable = x.right;
+
+        // Perform rotation
+        x.right = y;
+        y.left = swapVariable;
+
+        // Update heights
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        // Return new root
+        return x;
+    }
+
+    /**
+     * Rotates a node on the AVL tree left.
+     *
+     * @param x The node to perform the rotation on.
+     * @return The new root node after rotation.
+     */
+    private AVLNode leftRotate(AVLNode x) {
+        AVLNode y = x.right;
+        AVLNode swapVariable = y.left;
+
+        // Perform rotation
+        y.left = x;
+        x.right = swapVariable;
+
+        // Update heights
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+
+        // Return new root
+        return y;
+    }
 }
 
 public class GenericsKbAVLApp {
